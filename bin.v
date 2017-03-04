d0cbb4ee120e4ed95de4a22ef90dc2880a0f85c3
Esum <Esum0@hotmail.com>
2017-03-04 20:21:03 +0100

    Cleaned unused proofs.

--- bin.v
+++ bin.v
@@ -4,8 +4,9 @@ Inductive bin : Set :=
   | Zero : bin
   | Double : bin -> bin
   | DoubleOne : bin -> bin.
+(* A non negative integer can have many bin representation : 0 is represented by Zero, Double Zero, ... *)
 
-
+(** binS : bin -> bin, successor of the binary number b *)
 Fixpoint binS b :=
   match b with
   | Zero => DoubleOne Zero
@@ -13,12 +14,14 @@ Fixpoint binS b :=
   | DoubleOne d => Double (binS d)
   end.
 
+(** f : nat -> bin, converts a nat to a bin by iterating binS *)
 Fixpoint f n :=
   match n with
   | 0 => Zero
   | S m => binS (f m)
   end.
 
+(** g : bin -> nat, converts a bin to a nat *)
 Fixpoint g b :=
   match b with
   | Zero => 0
@@ -26,6 +29,7 @@ Fixpoint g b :=
   | DoubleOne d => S (g d + g d)
   end.
 
+(** h : bin -> bin, return a bin in a unique form without trailing Double Zero *)
 Fixpoint h b :=
   match b with
   | Zero => Zero
@@ -50,7 +54,6 @@ Proof.
   omega.
 Qed.
 
-
 Theorem g_f_bij : forall n, g (f n) = n.
 Proof.
   induction n.
@@ -65,9 +68,11 @@ Proof.
 
   rewrite <- IHn.
   simpl.
-  rewrite (g_succ b).
+  rewrite g_succ.
   omega.
 Qed.
+
+
 
 Theorem h_invar : forall b, g b = g (h b).
 Proof.
@@ -81,25 +86,6 @@ Proof.
   now rewrite IHb.
 Qed.
 
-Lemma neq_bins_0 : forall b, binS b <> Zero.
-Proof.
-  intros b H.
-  pose proof (g_succ b).
-  rewrite H in H0.
-  now assert (g Zero = 1).
-Qed.
-
-
-Lemma f_0_zero : forall n, f n = Zero <-> n = 0.
-Proof.
-  induction n.
-  easy.
-
-  simpl.
-  split; intro.
-  now pose proof (neq_bins_0 (f n)).
-  easy.
-Qed.
 
 
 Lemma f_add : forall n, n <> 0 -> f (n + n) = Double (f n).
@@ -110,96 +96,12 @@ Proof.
   simpl.
   rewrite <- (Nat.add_succ_comm n n).
   simpl.
-  pose proof(Nat.eq_decidable n 0).
-  unfold Decidable.decidable in H0.
-  destruct H0 as [H0|H0].
-  now rewrite H0.
+  destruct n.
+  now simpl.
+  assert (S n <> 0).
+  easy.
   apply IHn in H0.
   now rewrite H0.
-Qed.
-
-Lemma add_g : forall b, g (Double b) = g b + g b.
-Proof. 
-  now induction b.
-Qed.
-
-Lemma h_double : forall b, Double b = h (Double b) -> b = h b.
-Proof.
-  intros.
-  simpl in H.
-
-  destruct (h b).
-  easy.
-
-  now injection H.
-  now injection H.
-Qed.
-
-Lemma h_double_one : forall b, DoubleOne b = h (DoubleOne b) -> b = h b.
-Proof.
-  intros.
-  simpl in H.
-
-  now destruct (h b) ; injection H.
-Qed.
-
-Lemma neq_binS_zero : forall b, binS b <> Zero.
-Proof.
-  intro.
-  now destruct b.
-Qed.
-
-Lemma binS_invar : forall b, h b = b -> h (binS b) = (binS b).
-Proof.
-  induction b.
-  easy.
-  intros.
-  pose proof (eq_sym H) as H'.
-  apply (h_double b) in H'.
-  now rewrite H' at 2.
-
-  intros.
-  pose proof (eq_sym H) as H'.
-  apply (h_double_one b) in H'.
-  pose proof (eq_sym H') as H''.
-  apply IHb in H''.
-  simpl binS.
-  destruct (binS b) as []eqn:?.
-  now pose proof (neq_binS_zero b).
-  rewrite <- Heqb0 at 1.
-  simpl.
-  rewrite Heqb0.
-  now rewrite H''.
-  
-  rewrite <- Heqb0 at 1.
-  simpl.
-  rewrite Heqb0.
-  now rewrite H''.
-Qed.
-
-
-
-Lemma h_f_invar : forall n, f n = h (f n).
-Proof.
-  induction n.
-  easy.
-
-  simpl.
-  destruct (f n).
-  easy.
-  simpl.
-  apply (h_double b) in IHn.
-  now rewrite IHn at 1.
-  simpl binS.
-  apply (h_double_one b) in IHn.
-  pose proof (eq_sym IHn) as H.
-  apply (binS_invar b) in H.
-  simpl.
-  rewrite H.
-  destruct (binS b) as []eqn:?.
-  now pose proof (neq_binS_zero b).
-  reflexivity.
-  reflexivity.
 Qed.
 
 Lemma g_h_zero : forall b, g (h b) = 0 -> h b = Zero.
@@ -225,7 +127,6 @@ Proof.
   easy.
 Qed.
 
-
 Theorem f_g_h_bij : forall b, f (g (h b)) = h b.
 Proof.
   induction b.
@@ -235,32 +136,37 @@ Proof.
   destruct (h b) as []eqn:?.
   easy.
   simpl.
-  destruct (Nat.eq_decidable (g b0 + g b0) 0) as [H|H].
-  rewrite H.
+  destruct (g b0 + g b0) as []eqn:?.
   simpl.
   simpl in IHb.
-  rewrite H in IHb.
+  rewrite Heqn in IHb.
   simpl in IHb.
   easy.
-  apply (f_add (g b0 + g b0)) in H.
+  assert (S n <> 0).
+  easy.
+  apply (f_add (S n)) in H.
   rewrite H.
-  rewrite <- (add_g b0).
+  rewrite <- Heqn.
+  assert (g b0 + g b0 = g (Double b0)).
+  easy.
+  rewrite H0.
   now rewrite IHb.
 
-  rewrite (add_g (DoubleOne b0)).
+  assert (g (Double (DoubleOne b0)) = g (DoubleOne b0) + g (DoubleOne b0)).
+  easy.
+  rewrite H.
   assert (g (DoubleOne b0) <> 0).
   easy.
-  apply (f_add (g (DoubleOne b0))) in H.
-  rewrite H.
+  apply (f_add (g (DoubleOne b0))) in H0.
+  rewrite H0.
   now rewrite IHb.
 
   simpl.
-  destruct (Nat.eq_decidable (g (h b)) 0) as [H|H].
+  destruct (g (h b)).
+  now rewrite <- IHb.
+  assert (S n <> 0).
+  easy.
+  apply (f_add (S n)) in H.
   rewrite H.
-  simpl.
-  apply (g_h_zero b) in H.
-  now rewrite H.
-  apply (f_add (g (h b))) in H.
-  rewrite H.
-  now rewrite IHb.
+  now rewrite <- IHb.
 Qed.
